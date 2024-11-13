68fb77b15719037994243ac0ed02aa8a3be5d0df
Kovacs_Roland_Gergo <kovaxrawland@mailbox.unideb.hu>
2024-11-13 19:40:12 +0200

    feat: added extra protection from various attacks

--- app.js
+++ app.js
@@ -3,15 +3,57 @@ const toursRouter = require('./Routers/tourRouter.js');
 const userRouter = require('./Routers/userRouter.js');
 const AppError = require('./Utils/appError.js');
 const app = express();
+const helmet=require('helmet');
+const mongoSanitize=require('express-mongo-sanitize');
+const xss=require('xss-clean');
+const rateLimit=require('express-rate-limit');
 const globalErrorHandler = require('./Controllers/errorController.js');
+var cookieParser = require('cookie-parser');
+const hpp=require('hpp');
+const reviewRouter = require('./Routers/reviewRouter');
 
-//Middleware!?!
-app.use(express.json());
+app.use(helmet());
 
-const port=3000;
+//Max json data limit
+app.use(express.json({limit:'10kb'}));
+
+
+app.use(cookieParser());
+
+const limiter=rateLimit({
+  max:100,
+  windowMs:60*60*1000,
+  message:'Too many requests try again later!'
+});
+
+//DDOS protection
+app.use('/api',limiter);
+
+//INJECTION PROTECTION
+app.use(mongoSanitize());
+
+//Data Sanitization
+app.use(xss());
+
+//prevent parameter pollution whi
+app.use(hpp({
+  whitelist:['duration','difficulty','price','ratingsAverage','ratingsQuantity','maxGroupSize']
+}));
+
+//HTTP header defence
+app.use(helmet())
+
+//ROUTES
+app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', userRouter);
 app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next)=>{
+  req.requestTime=new Date().toISOString();
+  console.log(req.headers);
+  next();
+});
 
 
 app.all('*', (req, res, next) => {
