da9756f5885068b6e4098a0a720921eb2358dc21
Kovacs_Roland_Gergo <kovaxrawland@mailbox.unideb.hu>
2025-01-04 20:28:56 +0200

    feat: created automatic emails, after registering and after attempting to reset the password via email. Using MAILTRAP.

--- Utils/email.js
+++ Utils/email.js
@@ -1,24 +1,61 @@
-const nodemailer=require('nodemailer');
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text'); // transforms the html to a text
 
-const  sendMailer=async options=>
-{
-  //Host a create transportnal az adott cim, adott protjara, user credentialokkal kudi a datat
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Jonas <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport = function () {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
     }
-  });
-
-  const mailOptions = {
-    from: 'Kovacs Roland <testadmin@io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
+    return nodemailer.createTransport({
+      //everything here is received from nodemailer.
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
   };
-  await transporter.sendMail(mailOptions);
-};
 
-module.exports={sendMailer};
+  send = async function (template, subject) {
+    // 1) render HTML based on pug template
+    const html = pug.renderFile(
+      `${__dirname}/../view/email/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    // 2) define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    // 3) Create a transport to send email
+    await this.newTransport().sendMail(mailOptions);
+  };
+  sendWelcome = async function () {
+    await this.send('welcome', `Welcome to Natours ${this.firstName}!`);
+  };
+
+  sendPasswordReset= async function () {
+    await this.send('passwordReset', `Your password reset token (valid for only 10 minutes)!`);
+  }
+};
+module.exports={
+  Email
+}
